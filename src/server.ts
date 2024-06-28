cc9efb16bfb3341aaf7a8ad078dd03a7c5a0f71e
AEscobarAngola <abrancho1908@gmail.com>
2024-06-28 11:58:36 +0200

    feat: adding new kinds or requests

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,21 @@ app.get('/healthy', (req, res) => {
     res.send('server is healthy')
 })
 
+app.get('/books', (req, res) => {
+    res.send('get all books')
+})
 
+app.post('/books', (req, res) =>{
+    res.send('Book created')
+})
+
+app.put('/book', (req, res) => {
+    res.send('Book updated')
+})
+
+app.delete('/book', (req, res) => {
+    res.send('books deleted')
+})
 
 app.listen(4000, () => {
     console.log(`Server is running on port ${PORT}`);
