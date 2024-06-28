e5b4b735f549909e97d4f52a31f5ec2b972437ef
AEscobarAngola <abrancho1908@gmail.com>
2024-06-28 12:41:49 +0200

    feat: authors CRUD

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,43 @@ import express from 'express';
 
 const app = express();
 
+///middleware
+
+app.use(express.json())
+
 const PORT = process.env.PORT || 4000;
 
 app.get('/healthy', (req, res) => {
     res.send('server is healthy')
 })
+
+///AUTHORS
+
+app.post('/authors', (req, res) =>{
+    // recueperar la información de la req
+    console.log(req.body)
+    console.log(req.body.nationality)
+    console.log(req.body.name)
+
+
+    res.send('Create authors')
+})
+
+
+//// Rutas dinamicas req params /:.....
+app.put('/authors/:id', (req, res) =>{
+    console.log(req.body.id)
+
+    res.send(`author update with ${req.params.id}`)
+})
+
+app.delete('/author/:id', (req, res) => {
+
+    res.send(`author ${req.params.id} was deleted`)
+})
+
+
+/// BOOKS
 
 app.get('/books', (req, res) => {
     res.send('get all books')
