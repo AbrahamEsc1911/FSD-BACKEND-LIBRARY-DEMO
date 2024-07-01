b6126dc8e3a4d292b94e0ed6a631e8877cdf2d42
AEscobarAngola <abrancho1908@gmail.com>
2024-07-01 13:08:58 +0200

    feat: migrations user added to DB

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import 'dotenv/config'
 import { DataSource } from "typeorm"
 import { Authors1719825047836 } from "./migrations/1719825047836-authors"
+import { User1719825118738 } from "./migrations/1719825118738-user"
 
 export const AppDataSource = new DataSource({
 type: "mysql",
@@ -11,7 +12,7 @@ username: process.env.DB_USERNAME,
 password: process.env.DB_PASWORD,
 database: process.env.DB_DATABASE,
 entities: [],
-migrations : [Authors1719825047836],
+migrations : [Authors1719825047836, User1719825118738],
 synchronize: false,
 logging: false,
 })
--- src/database/migrations/1719825118738-user.ts
+++ src/database/migrations/1719825118738-user.ts
@@ -1,11 +1,69 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class User1719825118738 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "User",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        length: "50",
+                        isNullable: false,
+                        isUnique: true
+                    },
+                    {
+                        name: "password",
+                        type: "varchar",
+                        length: "50",
+                        isNullable: false
+                    },
+                    {
+                        name: "is_active",
+                        type: "Boolean",
+                        default: true,
+                        isNullable: false
+                    },
+                    {
+                        name: "role",
+                        type: "enum",
+                        enum: ['user', 'admin', 'super_admin'],
+                        default: "'user'"
+                    },
+                    {
+                        name: "created_at",
+                        type: "datetime",
+                        default: "now()"
+
+                    },
+                    {
+                        name: "updated_at",
+                        type: "datetime",
+                        default: "now()",
+                        onUpdate: "now()"
+                    },
+                ],
+            }),
+            true
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('user')
     }
 
 }
